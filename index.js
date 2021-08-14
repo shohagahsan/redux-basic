c8adde43084540916330f5046743d34c3a552151
Shohag Ahsan <theshohagahsan@gmail.com>
2021-08-15 01:47:10 +0600

    Redux Basic Chapter Completed

---
+++ index.js
@@ -0,0 +1,55 @@
+const redux = require('redux');
+
+// console.log(redux);
+
+// State
+const initState = {
+    num: 0
+}
+
+// Reducer
+const rootReducer = (state = initState, action) => {
+    if (action.type === "INC_NUM") {
+        return {
+            ...state,
+            num: state.num + 1
+        }
+    }
+    if (action.type === "ADD_NUM") {
+        return {
+            ...state,
+            num: state.num + action.a
+        }
+    }
+    if (action.type === "DEC_NUM") {
+        return {
+            ...state,
+            num: state.num -1
+        }
+    }
+    return state;
+}
+
+// Store
+const store = redux.createStore(rootReducer);
+
+console.log(store.getState());
+
+// Subscribe
+store.subscribe(() => {
+    console.log("[Subscribe]", store.getState());
+})
+
+// Dispatching Action
+store.dispatch({
+    type: "ADD_NUM",
+    a: 34
+});
+
+store.dispatch({
+    type: "INC_NUM"
+});
+
+store.dispatch({
+    type: "DEC_NUM"
+})
